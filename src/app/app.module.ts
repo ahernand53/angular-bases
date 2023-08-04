07439f44c97419bcfeb4f97c754b46f303ec8284
Asdrubal Hernandez <ahernand.development@gmail.com>
2023-08-03 20:51:19 -0500

    Implementaciones de la seccion #5
    Se realiza las implementaciones de toda la seccion #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [HeroesModule, BrowserModule, CounterModule],
+  imports: [HeroesModule, BrowserModule, CounterModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
